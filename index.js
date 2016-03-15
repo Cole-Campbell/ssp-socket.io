0dcd93fbf374d8f9dd7168dd18e7009487c26ed8
cole-campbell <cole.william.campbell@gmail.com>
2016-03-15 13:08:41 +0000

    Socket.io outputs message input to console, needs to be tinkered with to get output in window.

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + m);
-  });
+app.get('/', function (req,res){
+    res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *: 3000')
+})
+
+
+//Send to everyone
+io.emit('some event', {for: 'everyone'});
+
+//Sned to everyone except for a certainsocket
+io.on('connection', function(socket){
+    socket.broadcast.emit('hi');
+});
+
+io.on('conection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
 });
